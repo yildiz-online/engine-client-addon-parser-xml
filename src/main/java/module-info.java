0b26488b8eec0721bfc6f7ea9af795ec4ab35795
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-06-05 17:10:11 +0200

    [WIP] Remove dependency to common exception.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -31,9 +31,6 @@ module be.yildizgames.engine.client.addon.parser.xml {
     requires be.yildizgames.common.shape;
     requires be.yildizgames.common.file;
     requires be.yildizgames.module.audio;
-    requires be.yildizgames.common.logging;
-    requires be.yildizgames.common.exception;
-    requires be.yildizgames.common.util;
     requires java.xml;
     requires be.yildizgames.common.file.xml;
     requires be.yildizgames.module.window;
